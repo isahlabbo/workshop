56dda03cf4187acfd312d101b9e46dd6f5bc9e14
isah labbo <isahlabbo22@gmail.com>
2025-04-10 13:49:54 +0100

    make workshop_id nullable

--- database/migrations/2025_04_10_124824_add_workshop_id_to_applications.php
+++ database/migrations/2025_04_10_124824_add_workshop_id_to_applications.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ModifyApplicatiions extends Migration
+class AddWorkshopIdToApplications extends Migration
 {
     /**
      * Run the migrations.
@@ -25,6 +25,8 @@ class ModifyApplicatiions extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('applications', function (Blueprint $table) {
+            //
+        });
     }
 }
